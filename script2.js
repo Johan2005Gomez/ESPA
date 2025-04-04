f9029b65e89e6c944c36fec5d65ae109280e0295
Johan <johangomezaguirre@gmail.com>
2025-04-04 15:10:54 -0500

    primer commit

---
+++ script2.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var scrollTopButton = document.createElement("button");
+    scrollTopButton.id = "scrollTop";
+    scrollTopButton.innerHTML = "▲"; // Flecha hacia arriba
+    document.body.appendChild(scrollTopButton);
+
+    // Mostrar la flecha cuando el usuario hace scroll
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) {
+            scrollTopButton.classList.add("show");
+        } else {
+            scrollTopButton.classList.remove("show");
+        }
+    });
+
+    // Evento para hacer scroll hacia arriba
+    scrollTopButton.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
